98d5dcc88bee8fd1a30b819eece0850180c4c68c
Daniel Henry <email378637@gmail.com>
2018-05-24 09:24:54 -0700

    Update control.py

--- control.py
+++ control.py
@@ -2,8 +2,8 @@ import requests
 
 #KEY:
 
-#(Forward, Relay = 1), (Backward, Relay = 0), (No Acceleration, Stop = 1),
-#(Left, steerAngle = 60), (Right, steerAngle = 100), (Straight, steerAngle = 80) ~ 70)
+#(Forward, Relay = 1), (Backward, Relay = 0), (No Acceleration, Relay = 2),
+#(Left, steerAngle = 70), (Right, steerAngle = 95), (Straight, steerAngle = 70) ~ 70)
 
 class car:
     def __init__(self, espURL, debug=False):
@@ -14,27 +14,28 @@ class car:
                         'Stop': None
                         }
     def forward(self):
-        self.payload['Relay'] = 1
+        self.payload['Relay'] = 0
         r = requests.get(self.url, params=self.payload)
         if self.debug:
             print(r.text, '\n')  
     def backward(self):
-        self.payload['Relay'] = 0
+        self.payload['Relay'] = 1
         r = requests.get(self.url, params=self.payload)
         if self.debug:
             print(r.text, '\n')  
     def stop(self):
         self.payload['Relay'] = 2
+        self.payload['steerAngle'] = 70
         r = requests.get(self.url, params=self.payload)
         if self.debug:
             print(r.text, '\n')  
     def left(self):
-        self.payload['steerAngle'] = 60
+        self.payload['steerAngle'] = 45
         r = requests.get(self.url, params=self.payload)
         if self.debug:
             print(r.text, '\n') 
     def right(self):
-        self.payload['steerAngle'] = 100
+        self.payload['steerAngle'] = 95
         r = requests.get(self.url, params=self.payload)
         if self.debug:
             print(r.text, '\n') 
