231fdf8c1918be79712f1817fcacf053327f3506
Daniel Henry <email378637@gmail.com>
2018-05-25 20:02:02 -0700

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -6,10 +6,10 @@ from alexnet import alexnet
 import random
 from win32 import win32api
 
-WIDTH = 80
-HEIGHT = 60
+WIDTH = 160
+HEIGHT = 120
 LR = 1e-3
-EPOCHS = 10
+EPOCHS = 15
 MODEL_NAME = 'alexnet_model/rc-{}-{}-{}-epochs.model'.format(LR, 'alexnetv2',EPOCHS)
  
 model = alexnet(WIDTH, HEIGHT, LR)
@@ -22,32 +22,35 @@ def main():
         time.sleep(1)
 
     paused = False
-    #IP camera video stream URL
-    cam_url = 'http://192.168.8.100:8080/video'
+    cam_url = 'http://192.168.8.110:8080/video'
     cam = cv2.VideoCapture(cam_url)
-    #ESP 8266 server URL
     test_rig = car('http://192.168.8.108/controls')
     
     while(True):
+        
         if not paused:
+            cam = cv2.VideoCapture(cam_url)
             ret, screen = cam.read()
             last_time = time.time()
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-            screen = cv2.resize(screen, (80,60))
+            screen = cv2.resize(screen, (WIDTH, HEIGHT))
 
-            prediction = model.predict([screen.reshape(80,60,1)])[0]
+            prediction = model.predict([screen.reshape(WIDTH, HEIGHT,1)])[0]
             print(prediction)
 
-            turn_thresh = .75
-            fwd_thresh = 0.70
+            turn_thresh = 0.75
+            fwd_thresh = 0.50
 
-            if prediction[0] > turn_thresh:
+            if prediction[1] > turn_thresh and max(prediction) == prediction[1]:
+                print('LEFT\n')
                 test_rig.left()
                 test_rig.forward()
-            elif prediction[2] > turn_thresh:
+            elif prediction[2] > turn_thresh and max(prediction) == prediction[2]:
+                print('RIGHT\n')
                 test_rig.right()
                 test_rig.forward()
-            elif prediction[1] > fwd_thresh:
+            elif prediction[0] > fwd_thresh:
+                print('FORWARD\n')
                 test_rig.forward()
             else:
                 test_rig.stop()
